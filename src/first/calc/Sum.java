77d01f539e0bde06d9de45c1c38e923e7fb966cc
Narrya <kamila.kupidura@gmail.com>
2018-01-08 21:48:28 +0100

    CR fixes

--- src/first/calc/Sum.java
+++ src/first/calc/Sum.java
@@ -4,7 +4,7 @@ public class Sum extends OperationMultiArgs {
 	public double execute() {
 		double sum = 0.0;
 
-		for (int i = 0; i < this.count; ++i){
+		for (int i = 0; i < this.count; ++i) {
 			sum = sum + this.arguments[i];
 		}
 
